3aeea24901a683176c7d64cbd1c413de7b064b9a
mac <mac@Naisss-2.local>
2026-01-28 20:11:41 +0700

    update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -75,7 +75,7 @@ export default function Sidebar({ userName, permissions }: SidebarProps) {
 
       <button
         onClick={handleLogout}
-        className="m-4 py-2 px-4 bg-red-600 hover:bg-red-700 rounded text-sm transition-colors"
+        className="m-4 py-2 px-4 bg-white text-black hover:bg-red-700 hover:text-white rounded text-sm transition-colors"
       >
         Logout
       </button>
