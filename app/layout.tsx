e19d275309db0f7251a878556ff369871334f7f8
mac <mac@Naisss-2.local>
2026-07-23 14:14:22 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/ThemeContext";
 import { SidebarProvider } from "@/context/SidebarContext";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -51,7 +50,6 @@ export default function RootLayout({
             {children}
           </SidebarProvider>
         </ThemeProvider>
-        <SpeedInsights />
       </body>
     </html>
   );
